64e8d7537d14595afff935fb52618f5944516c33
Arstotzk <50625444+Arstotzk@users.noreply.github.com>
2021-11-05 11:33:54 +0700

    Add resize image

--- MainMini.py
+++ MainMini.py
@@ -8,10 +8,10 @@ def culcSum(_x, _y, _img, _shab):
     summ = 0
     xs = -1
     ys = -1
-    for x in range (_x - 64, _x + 64):
+    for x in range (_x - 8, _x + 8):
         xs += 1
         ys = -1
-        for y in range (_y - 64, _y + 64):
+        for y in range (_y - 8, _y + 8):
             ys += 1
             r = _img.getpixel((x, y))
             rs = _shab.getpixel((xs, ys))
@@ -21,38 +21,49 @@ def culcSum(_x, _y, _img, _shab):
                 summ += 1/((r - rs) ** 2)
     return summ
 
+def resizeImg(_img):
+    width, height = _img.size
+    new_width = int(width/8)
+    new_height = int(height/8)
+    print(new_width, new_height)
+    _img = _img.resize((new_width, new_height), Image.ANTIALIAS)
+    return _img
 
-img = Image.open("img/1.jpg")
+img = Image.open("img/1prepare.jpg")
 img = img.convert('L')
-imgSum = Image.open("img/1.jpg")
+img = resizeImg(img)
+imgSum = Image.open("img/1prepare.jpg")
 imgSum = imgSum.convert('L')
+imgSum = resizeImg(imgSum)
 shab = Image.open("img/1S.png")
 shab = shab.convert('L')
+shab = resizeImg(shab)
 width, height = img.size
 arrSumm = np.full((width, height), 0)
 save('arrZero.npy', arrSumm)
 summOld = 0
-for x in range (64, width-64):
-    for y in range (64, height-64):
+for x in range (8, width-8):
+    for y in range (8, height-8):
         summ = culcSum(x, y, img, shab)
         arrSumm[x, y] = summ
         if (summ > summOld):
             xPoint = x
             yPoint = y
-    print("Ready: " + str(((x - 63)/(width-128)) * 100) + "%")
-save('arrSumm2.npy', arrSumm)
+    print("Ready: " + str(((x - 7)/(width-16)) * 100) + "%")
+save('arrSummMini.npy', arrSumm)
 maxArrSum = 0
-for x in range (64, width-64):
-    for y in range (64, height-64):
+for x in range (8, width-8):
+    for y in range (8, height-8):
         nextArrSum = arrSumm[x, y]
         if (nextArrSum > maxArrSum):
             maxArrSum = nextArrSum
-for x in range (64, width-64):
-    for y in range (64, height-64):
+for x in range (8, width-8):
+    for y in range (8, height-8):
         arrSumm[x, y] = int(((arrSumm[x, y])/maxArrSum) * 255)
-for x in range (64, width-64):
-    for y in range (64, height-64):
-        imgSum.putpixel((x, y)(arrSumm[x, y], arrSumm[x, y], arrSumm[x, y]))
+for x in range (8, width-8):
+    for y in range (8, height-8):
+        collor = int(arrSumm[x, y].item())
+        imgSum.putpixel((x, y),(collor))
 print(xPoint, yPoint)
 imgSum.show()
 img.putpixel((xPoint,yPoint)(0, 255, 0))
