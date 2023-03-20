1586153ad3bd7a8cf4b6b58671a927fb6c19361e
Arstotzk <50625444+Arstotzk@users.noreply.github.com>
2023-03-20 13:25:58 +0700

    #25 AddTestFindPoints

---
+++ tests/test_image_operations.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import config_read
+from normalization import Normalization
+from image_operations import ImageOperations
+
+
+if __name__ == '__main__':
+    image = Image.open("img/1prepare2.jpg")
+    norm = Normalization(image)
+    norm.normalize()
+    imageOper = ImageOperations(norm.imgNormalize, config_read.Settings())
+    imageOper.find_points_by_template()
+    imageOper.find_cephalometric_params()
+    imageOper.print_cephalometric_params()
+    print("Executing time: " + str(imageOper.execution_time()))
+    imageOper.set_points_on_image()
+    imageOper.image.show()
